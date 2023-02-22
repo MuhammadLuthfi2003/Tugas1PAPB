53c9655a4d8031916fa75351b4938ab99e3d6744
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2023-02-22 11:50:48 +0700

    fragment statis

---
+++ app/src/main/java/com/example/tugas1fragment/ButtonPindah.java
@@ -0,0 +1,40 @@
+package com.example.tugas1fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ButtonPindah} factory method to
+ * create an instance of this fragment.
+ */
+public class ButtonPindah extends Fragment {
+
+    public ButtonPindah() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_button_pindah, container, false);
+        Button pindahButton = rootView.findViewById(R.id.button_fragment);
+
+        pindahButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return rootView;
+    }
+}
